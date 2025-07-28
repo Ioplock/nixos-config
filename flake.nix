79e46f88d9baccaa14e216a3ca28517e4d78926f
Ioplock <ioplock.me@gmail.com>
2025-07-28 16:11:20 +0300

    Init

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+
+  description = "My system configuration :3";
+  
+  inputs = {
+	nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
+ 	home-manager = {
+		url = "github:nix-community/home-manager/release-25.05";
+		inputs.nixpkgs.follows = "nixpkgs";
+	};
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+	let
+		libNP = nixpkgs.lib;
+		libHM = home-manager.lib;
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+	in {
+  	nixosConfigurations = {
+		nixos-ioplock = libNP.nixosSystem {
+			inherit system;
+			modules = [ ./configuration.nix ];
+		};
+	};
+	homeConfigurations = {
+		ioplock = libHM.homeManagerConfiguration {
+			inherit pkgs;
+			modules = [ ./home.nix ];
+		};
+	};
+  };
+
+}
