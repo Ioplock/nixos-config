9286b29ec06517289a5ea3d94ebe81585bd96b0b
Ioplock <ioplock.me@gmail.com>
2025-07-30 09:25:50 +0300

    Add Ghostty to exec-once list for improved terminal management

--- home-manager/modules/hyprland/main.nix
+++ home-manager/modules/hyprland/main.nix
@@ -21,6 +21,7 @@
 
       exec-once = [
         "hyprlock"
+        "ghostty --gtk-single-instance=true --quit-after-last-window-closed=false --initial-window=false"
       ];
 
       # exec-once = [
