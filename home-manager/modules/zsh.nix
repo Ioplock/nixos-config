9916a4ba9b0ad98e418869c9210dbd85595f0623
Ioplock <ioplock.me@gmail.com>
2025-07-30 08:47:53 +0300

    Refactor Zsh initialization script to use 'initContent' and remove 'hyprlock' command
    - Changed 'initExtra' to 'initContent' for clarity in the Zsh module.
    - Removed the 'hyprlock' command from the initialization script.

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -5,13 +5,11 @@
 		autosuggestion.enable = true;
 		syntaxHighlighting.enable = true;
 
-		initExtra = ''
+		initContent = ''
 			# Start UWSM
       if uwsm check may-start > /dev/null; then
         exec systemd-cat -t uwsm_start uwsm start default
       fi
-
-			hyprlock
     '';
 	};
 }
