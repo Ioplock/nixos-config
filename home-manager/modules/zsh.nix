fc6ded16d2bfbb15776f01762884e3b0f4db0a23
Ioplock <ioplock.me@gmail.com>
2025-07-30 08:22:12 +0300

    Refactor UWSM initialization in Zsh module
    - Simplified the UWSM start condition by removing the 'uwsm select' command from the initialization script.

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -7,7 +7,7 @@
 
 		initExtra = ''
 			# Start UWSM
-      if uwsm check may-start > /dev/null && uwsm select; then
+      if uwsm check may-start > /dev/null; then
         exec systemd-cat -t uwsm_start uwsm start default
       fi
     '';
