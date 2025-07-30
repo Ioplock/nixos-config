609c2cf1ca976c9c15f131f72d4c717de834fb6f
Ioplock <ioplock.me@gmail.com>
2025-07-30 09:11:49 +0300

    Add Ghostty settings for enhanced terminal integration
    - Enabled Bash integration alongside existing Zsh integration.
    - Updated Ghostty settings with new cursor style, background opacity, and blur radius.
    - Configured window settings for improved user experience.

--- home-manager/modules/ghostty.nix
+++ home-manager/modules/ghostty.nix
@@ -2,8 +2,23 @@
   programs.ghostty = {
     enable = true;
     enableZshIntegration = true;
+    enableBashIntegration = true;
+
     settings = {
       theme = "catppuccin-mocha";
+
+      cursor-style = "bar";
+      cursor-style-blink = "true";
+
+      background-opacity = "0.8";
+      background-blur-radius = "20";
+
+      window-vsync = "false";
+      window-decoration = "false";
+
+      window-save-state = "never";
+      
+      shell-integration = "zsh";
     };
   };
 }
