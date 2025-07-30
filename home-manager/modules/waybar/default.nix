b5ee54238e77ec99fa9d5c7db2bc772a1628aaa5
Ioplock <ioplock.me@gmail.com>
2025-07-30 11:30:14 +0300

    Update Waybar formatting for network and battery modules
    - Changed icons for Wi-Fi, Ethernet, and disconnected states to enhance visual representation.
    - Updated battery format icons for improved clarity and aesthetics.

--- home-manager/modules/waybar/default.nix
+++ home-manager/modules/waybar/default.nix
@@ -23,9 +23,9 @@
         };
 
         "network" = {
-          format-wifi = "  {signalStrength}%";
-          format-ethernet = " {ifname}";
-          format-disconnected = "⚠ Disconnected";
+          format-wifi = "󰤨 {signalStrength}%";
+          format-ethernet = "󰈀 {ifname}";
+          format-disconnected = "󰤭 Disconnected";
           tooltip-format-wifi = "Connected to {essid}";
           tooltip-format-ethernet = "Connected via ethernet";
           on-click = "nm-connection-editor";
@@ -62,7 +62,7 @@
           format = "{icon} {capacity}%";
           format-charging = " {capacity}%";
           format-alt = "{time} {icon}";
-          format-icons = ["" "" "" "" ""];
+          format-icons = ["󰂎" "󰁺" "󰁻" "󰁼" "󰁽"];
         };
 
         "clock" = {
