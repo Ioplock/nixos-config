a4d1e82e7bb574b54d1522a079be0ca62547afce
Ioplock <ioplock.me@gmail.com>
2025-08-01 05:04:53 +0300

    Enhance home-manager configuration with additional packages and keybindings
    - Added new desktop applications: vscode, code-cursor, imv, mpv, vesktop, and pavucontrol for improved user experience.
    - Introduced utilities such as bc, bottom, htop, inxi, playerctl, cliphist, wl-clipboard, brightnessctl, grimblast, udisks, and ntfs3g for enhanced system management.
    - Updated terminal tools with silicon, wget, and curl for better functionality.
    - Refined keybindings in Hyprland configuration for improved accessibility and usability.

--- home-manager/home-packages.nix
+++ home-manager/home-packages.nix
@@ -2,7 +2,28 @@
   nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
+
+    # Desktop
+    vscode # Code editor
+    code-cursor # Cursor IDE
+    imv # Image viewer
+    mpv # Video player
+    vesktop # Discord with built-in vencord
+    pavucontrol # Audio control
     
+    # Utilities
+    bc # Calculator
+    bottom # Process manager
+    htop # Process manager
+    inxi # System info
+    playerctl # Media player
+    cliphist # Clipboard history
+    wl-clipboard # Clipboard
+    brightnessctl # LCD brightness
+    grimblast # Screenshots
+    udisks # Access and manipulate disks
+    ntfs3g # NTFS support
+
     # Terminal tools
     fastfetch # System info
     jq # JSON processor
@@ -11,6 +32,9 @@
     tree
     eza # Better ls
     ffmpeg # Video&Audio processor
+    silicon # Code to image
+    wget # Downloader
+    curl # HTTP client
 
     # Rice stuff
     lavat # Lavalamp in terminal
@@ -23,7 +47,7 @@
 
     # Other
     bemoji
-    
+
   ];
 
 }
